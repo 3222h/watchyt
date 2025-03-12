3fca1da592bdde052581c6c39fd69635af81dd28
3222h <394954a+++@gmail.com>
2025-03-12 13:16:31 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,36 +1,6 @@
-let intervalId = null;
-const timeIntervals = [300000, 60000, 60000, 60000, 60000, 60000, 60000, 60000, 60000, 60000];
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === 'start') startSkipping(message.tabId);
-  else if (message.action === 'stop') stopSkipping();
-});
-
-function startSkipping(tabId) {
-  let count = 0;
-  function skip() {
-    if (count >= timeIntervals.length) {
-      stopSkipping();
-      return;
-    }
-    chrome.scripting.executeScript({
-      target: { tabId },
-      function: nextVideo
-    });
-    intervalId = setTimeout(skip, timeIntervals[count]);
-    count++;
-  }
-  skip();
-  chrome.runtime.sendMessage({ status: 'running' });
-}
-
-function stopSkipping() {
-  if (intervalId) clearTimeout(intervalId);
-  intervalId = null;
-  chrome.runtime.sendMessage({ status: 'stopped' });
-}
-
-function nextVideo() {
-  let nextButton = document.querySelector('.ytp-next-button');
-  if (nextButton) nextButton.click();
-}
+chrome.webNavigation.onCompleted.addListener(function (details) {
+  chrome.scripting.executeScript({
+    target: { tabId: details.tabId },
+    files: ['content.js']
+  });
+}, { url: [{ hostContains: 'youtube.com' }] });
