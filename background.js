dcb8a449e0d381bf66c8bd6a56e6f35faaa180f6
3222h <394954a+++@gmail.com>
2025-03-12 12:31:39 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,24 +1,4 @@
-let intervalId = null;
-const timeIntervals = [300000, 60000, 60000, 60000, 60000, 60000, 60000, 60000, 60000, 60000];
-
-function startSkipping(tabId) {
-  let count = 0;
-  intervalId = setInterval(() => {
-    chrome.scripting.executeScript({
-      target: { tabId },
-      function: nextVideo
-    });
-    count++;
-    if (count >= timeIntervals.length) stopSkipping();
-  }, timeIntervals[count]);
-}
-
-function stopSkipping() {
-  if (intervalId) clearInterval(intervalId);
-  intervalId = null;
-}
-
-function nextVideo() {
-  let nextButton = document.querySelector('.ytp-next-button');
-  if (nextButton) nextButton.click();
-}
+chrome.runtime.onMessage.addListener((message) => {
+  if (message.action === 'start') startSkipping(message.tabId);
+  else if (message.action === 'stop') stopSkipping();
+});
