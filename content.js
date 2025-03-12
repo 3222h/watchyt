77ae7d6c1b0aac97041fc33c0cdb812226fd70d5
3222h <394954a+++@gmail.com>
2025-03-12 17:57:21 +0530

    Update content.js

--- content.js
+++ content.js
@@ -5,7 +5,17 @@ function playNextVideo() {
   }
 }
 
-setTimeout(playNextVideo, 10000);
-setTimeout(playNextVideo, 20000);
-setTimeout(playNextVideo, 40000);
-setTimeout(playNextVideo, 30000);
+setTimeout(playNextVideo, 3600000);
+setTimeout(playNextVideo, 3840000);
+setTimeout(playNextVideo, 4020000);
+setTimeout(playNextVideo, 4200000);
+setTimeout(playNextVideo, 4380000);
+setTimeout(playNextVideo, 4560000);
+setTimeout(playNextVideo, 4740000);
+setTimeout(playNextVideo, 4920000);
+setTimeout(playNextVideo, 5100000);
+setTimeout(playNextVideo, 5280000);
+setTimeout(playNextVideo, 5460000);
+setTimeout(playNextVideo, 5640000);
+setTimeout(playNextVideo, 5820000);
+setTimeout(playNextVideo, 6000000);
