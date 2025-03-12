215063cbad7d5c2332d499ad4e5630fa5c864e1c
3222h <394954a+++@gmail.com>
2025-03-12 14:26:30 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,9 +1,17 @@
 function playNextVideo() {
-  const nextButton = document.querySelector('a.ytp-next-button');
+  console.log('Checking for next video button...');
+
+  const nextButton = document.querySelector('button.ytp-next-button') || document.querySelector('a.ytp-next-button');
+  
   if (nextButton) {
-    nextButton.click();
+    console.log('Next button found:', nextButton);
+    console.log('Is button visible?', nextButton.offsetParent !== null);
+    console.log('Clicking the next button...');
+
+    nextButton.dispatchEvent(new MouseEvent('click', { bubbles: true, cancelable: true }));
+  } else {
+    console.log('Next button not found.');
   }
 }
 
-// Play next video every 60 seconds
 setInterval(playNextVideo, 60000);
