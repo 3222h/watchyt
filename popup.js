59babdc7d23189deb8f26a1e6c07d808e062d781
3222h <394954a+++@gmail.com>
2025-03-12 12:29:57 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,3 +1,8 @@
-document.getElementById("startTimer").addEventListener("click", () => {
-  chrome.runtime.sendMessage({ action: "startTimer" });
+document.getElementById('start').addEventListener('click', async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  chrome.runtime.sendMessage({ action: 'start', tabId: tab.id });
+});
+
+document.getElementById('stop').addEventListener('click', () => {
+  chrome.runtime.sendMessage({ action: 'stop' });
 });
