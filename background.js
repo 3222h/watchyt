352fb90f76d4a489717af813384782a0cdd1f672
3222h <394954a+++@gmail.com>
2025-03-12 17:13:59 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,3 +1,14 @@
+chrome.runtime.onStartup.addListener(() => {
+  chrome.tabs.query({ url: "*://www.youtube.com/watch?v=*" }, (tabs) => {
+    tabs.forEach((tab) => {
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        files: ['content.js']
+      });
+    });
+  });
+});
+
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (tab.url && tab.url.includes('youtube.com/watch')) {
     chrome.scripting.executeScript({
