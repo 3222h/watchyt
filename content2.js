14e5d24e4dc8caceb74f9046913a3c0daaba293a
3222h <394954a+++@gmail.com>
2025-04-20 22:40:31 +0530

    Update content2.js

--- content2.js
+++ content2.js
@@ -1,6 +1,6 @@
 function playNextVideo() {
     console.log("Next video played using Shift + N. Generating random wait time...");
-    
+
     // Simulate Shift + N key press to play the next video
     document.dispatchEvent(new KeyboardEvent('keydown', {
         key: 'N',
@@ -9,10 +9,11 @@ function playNextVideo() {
         which: 78,
         shiftKey: true
     }));
-    
-    let waitTime = Math.floor(Math.random() * (120 - 60 + 1) + 60) * 1000;
+
+    // Random wait time between 35 to 45 seconds
+    let waitTime = Math.floor(Math.random() * (45 - 35 + 1) + 35) * 1000;
     console.log(`Waiting for ${waitTime / 1000} seconds before playing the next video.`);
-    
+
     setTimeout(playNextVideo, waitTime);
 }
 
@@ -24,11 +25,22 @@ function getYouTubeVideoId() {
 function checkVideoIdAndStart() {
     chrome.storage.sync.get(["specialVideoID", "waitTime"], function (data) {
         let specialVideoID = data.specialVideoID;
-        let waitTime = data.waitTime || 3600000; // Default to 1 hour (60 minutes) if not set
+        let userDefinedWait = data.waitTime;
+
+        let finalWaitTime;
 
         if (getYouTubeVideoId() === specialVideoID) {
-            console.log(`Video ID matched! Starting countdown of ${waitTime / 60000} minutes...`);
-            setTimeout(playNextVideo, waitTime);
+            // If user has set a wait time, add up to 30 seconds randomly
+            if (userDefinedWait && typeof userDefinedWait === "number") {
+                finalWaitTime = Math.floor(Math.random() * (30000 + 1)) + userDefinedWait; // waitTime to waitTime + 30s
+                console.log(`User-defined wait time: ${userDefinedWait / 60000} minutes. Actual wait: ${finalWaitTime / 1000} seconds.`);
+            } else {
+                // Default wait time between 2 to 2.5 minutes
+                finalWaitTime = Math.floor(Math.random() * (150 - 120 + 1) + 120) * 1000;
+                console.log(`No user-defined wait time. Using default: ${finalWaitTime / 60000} minutes.`);
+            }
+
+            setTimeout(playNextVideo, finalWaitTime);
         } else {
             console.log("Video ID does not match. Checking again in 5 seconds...");
             setTimeout(checkVideoIdAndStart, 5000);
